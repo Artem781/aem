6293ead5b9fb81df546fef191274913540e00ffa
Artem Naron <naronartem@gmail.com>
2022-02-09 00:10:38 +0300

    custom component without styles was added

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Image.java
@@ -0,0 +1,9 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+public class Image {
+    private Image getImage() {
+        Image image = null;
+        // Figure out how to populate the image variable!
+        return image;
+    }
+}
